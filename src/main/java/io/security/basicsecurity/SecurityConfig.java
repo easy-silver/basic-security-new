887ba333d8cd745303bb080d9fa2c535e5cb9d6a
Timo <developer.timo@gmail.com>
2022-10-19 23:23:00 +0900

    [스프링 시큐리티 기본 API 및 Filter 이해] 8) 익명사용자 인증 필터 : AnonymousAuthenticationFilter

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -4,26 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final UserDetailsService userDetailsService;
-
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //인증 정책
         http
                 .formLogin();
-        http
-                .rememberMe()
-                .userDetailsService(userDetailsService);
 
         //인가 정책
         http
