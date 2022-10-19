d580838715f458f9f9d42ec67a7ea46b9396f9a3
Timo <developer.timo@gmail.com>
2022-10-19 23:08:31 +0900

    [스프링 시큐리티 기본 API 및 Filter 이해] 7) Remember Me 인증 필터 : RememberMeAuthenticationFilter

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,13 +1,10 @@
 package io.security.basicsecurity;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
-import javax.servlet.http.HttpSession;
 
 @Configuration
 @EnableWebSecurity
@@ -23,46 +20,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //인증 정책
         http
-                .formLogin()
-//                .loginPage("/loginPage")
-                .defaultSuccessUrl("/")
-                .failureForwardUrl("/login")
-                //usernameParameter, passwordParameter, loginProcessingUrl 세 가지는 로그인 화면 UI 파라미터명과 맞춰야 한다.
-                .usernameParameter("userId")
-                .passwordParameter("passwd")
-                .loginProcessingUrl("/login_proc")
-                .successHandler((request, response, authentication) -> {
-                    System.out.println("authentication : " + authentication.getName());
-                    response.sendRedirect("/");
-                })
-                .failureHandler((request, response, exception) -> {
-                    System.out.println("exception : " + exception.getMessage());
-                    response.sendRedirect("/login");
-                })
-                .permitAll();
-
+                .formLogin();
         http
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .addLogoutHandler((request, response, authentication) -> {
-                    HttpSession session = request.getSession();
-                    session.invalidate();
-                })
-                .logoutSuccessHandler((request, response, authentication) -> {
-                    response.sendRedirect("/login");
-                })
-                .deleteCookies("remember-me")
-            .and()
                 .rememberMe()
-                .rememberMeParameter("remember")
-                .tokenValiditySeconds(3600)
                 .userDetailsService(userDetailsService);
 
         //인가 정책
         http
                 .authorizeRequests()
-                .anyRequest()
-                .authenticated();
+                .anyRequest().authenticated();
     }
 }
