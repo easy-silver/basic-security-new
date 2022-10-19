b932fc578ad4ea3f56a19d2d0415948173300cc9
Timo <developer.timo@gmail.com>
2022-10-19 11:04:26 +0900

    [스프링 시큐리티 기본 API 및 Filter 이해] 3) Form Login 인증

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -13,7 +13,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //인증 정책
         http
-                .formLogin();
+                .formLogin()
+                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureForwardUrl("/login")
+                //usernameParameter, passwordParameter, loginProcessingUrl 세 가지는 로그인 화면 UI 파라미터명과 맞춰야 한다.
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+                .successHandler((request, response, authentication) -> {
+                    System.out.println("authentication : " + authentication.getName());
+                    response.sendRedirect("/");
+                })
+                .failureHandler((request, response, exception) -> {
+                    System.out.println("exception : " + exception.getMessage());
+                    response.sendRedirect("/login");
+                })
+                .permitAll();
 
         //인가 정책
         http
