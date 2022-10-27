22a82f3d1d52ec87e2df2315bf3fe3fc99033c03
Timo <developer.timo@gmail.com>
2022-10-27 18:51:56 +0900

    [스프링 시큐리티 기본 API 및 Filter 이해] 11) 권한설정과 표현식
    - 권한 설정을 위한 계정 설정 및 경로별 인가 설정

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,6 +1,7 @@
 package io.security.basicsecurity;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,19 +11,33 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}1111")
+                .roles("USER")
+                .and()
+                .withUser("sys")
+                .password("{noop}1111")
+                .roles("SYS")
+                .and()
+                .withUser("admin")
+                .password("{noop}1111")
+                .roles("ADMIN");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         //인증 정책
         http
                 .formLogin();
 
-        http
-                .sessionManagement()
-                .maximumSessions(1)
-                .maxSessionsPreventsLogin(true);
-
         //인가 정책
         http
                 .authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 .anyRequest().authenticated();
     }
 }
