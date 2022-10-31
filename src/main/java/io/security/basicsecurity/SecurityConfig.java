69c63dfe80c55dcfabb9d6532c630adbc8ef9ca4
Timo <developer.timo@gmail.com>
2022-10-31 22:09:26 +0900

    [스프링 시큐리티 주요 아키텍처 이해] 4) 인증 저장소 - SecurityContextHolder, SecurityContext
    - 두 가지 방법으로 SecurityContext 에서 인증 정보 가져오기

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,37 +1,40 @@
 package io.security.basicsecurity;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Order(0)
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .antMatcher("/admin/**")
-                .authorizeRequests()
-                .anyRequest().authenticated()
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}1111")
+                .roles("USER")
                 .and()
-                .httpBasic();
+                .withUser("sys")
+                .password("{noop}1111")
+                .roles("SYS")
+                .and()
+                .withUser("admin")
+                .password("{noop}1111")
+                .roles("ADMIN");
     }
-}
-
-@Order(1)
-@Configuration
-class SecurityConfig2 extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
                 .and()
+                .httpBasic();
+
+        http
                 .formLogin();
     }
 }
--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,38 +1,24 @@
 package io.security.basicsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession session) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SecurityContext context = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication1 = context.getAuthentication();
+
         return "home";
     }
 
-    @GetMapping("/user")
-    public String user() {
-        return "user";
-    }
-
-    @GetMapping("/admin/pay")
-    public String adminPay() {
-        return "adminPay";
-    }
-
-    @GetMapping("/admin/**")
-    public String admin() {
-        return "admin";
-    }
-
-    @GetMapping("/denied")
-    public String denied() {
-        return "Access is Denied";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
 }
