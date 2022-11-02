1135d37be0f1ff821e9720412077b951f50e245a
Timo <developer.timo@gmail.com>
2022-11-02 20:52:34 +0900

    [스프링 시큐리티 주요 아키텍처 이해] 6) 인증 흐름 이해 - Authentication Flow

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,31 +1,13 @@
 package io.security.basicsecurity;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("{noop}1111")
-                .roles("USER")
-                .and()
-                .withUser("sys")
-                .password("{noop}1111")
-                .roles("SYS")
-                .and()
-                .withUser("admin")
-                .password("{noop}1111")
-                .roles("ADMIN");
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -34,8 +16,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         http
                 .formLogin();
-
-        //SecurityContext의 공유 범위 설정
-        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 }
